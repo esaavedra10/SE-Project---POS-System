af8915730274414089133111c3539db3b64d8272
esaavedra10 <znl19@txstate.edu>
2026-03-30 22:05:27 -0500

    Added all the current project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Point_of_Sales_System.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
