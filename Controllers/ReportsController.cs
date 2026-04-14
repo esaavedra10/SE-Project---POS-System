5e2e30e02d340f4772eaef3602f5b5767fcb31d4
Ashton Racine <cdj99@txstate.edu>
2026-04-14 13:16:59 -0500

    Add reporting feature

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SE_Project___POS_System.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
