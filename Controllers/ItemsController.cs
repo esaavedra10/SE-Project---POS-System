284b9675022a00dab42ee56543a72bb6f558b432
esaavedra10 <znl19@txstate.edu>
2026-04-23 00:33:23 -0500

    Feature branch for Product SKU Lookup

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoExample.Services;
+using MongoExample.Models;
+using MongoExample.Models.ViewModels;
+namespace MongoExample.Controllers;
 
-namespace SE_Project___POS_System.Controllers
+public class ItemsController : Controller
 {
-    public class ItemsController : Controller
+    private readonly ProductsServices _productsServices;
+
+    public ItemsController(ProductsServices productsServices)
     {
-        public IActionResult Lookup()
+        _productsServices = productsServices;
+    }
+
+    [HttpGet]
+    public IActionResult Lookup()
+    {
+        return View(new ItemLookupView());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Lookup(ItemLookupView model)
+    {
+        if (!string.IsNullOrWhiteSpace(model.SearchTerm))
         {
-            return View();
+            model.SelectedProduct = await _productsServices.GetBySkuAsync(model.SearchTerm);
         }
+
+        return View(model);
     }
 }
